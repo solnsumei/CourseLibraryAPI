c1d8b2952cbde4dbc13c068e0ce343b34536873f
solnsumei <solnsumei@gmail.com>
2020-08-21 12:15:25 +0100

    Update setup

--- CourseLibrary.API/Startup.cs
+++ CourseLibrary.API/Startup.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CourseLibrary.API.DbContexts;
+using CourseLibrary.API.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -26,6 +29,15 @@ namespace CourseLibrary.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddScoped<ICourseLibraryRepository, CourseLibraryRepository>();
+            
+            services.AddDbContext<CourseLibraryContext>(options =>
+            {
+                options.UseMySQL(
+                    "server=localhost;database=courseLibraryDb;user=solmei;password=solomon1");
+            }); 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
